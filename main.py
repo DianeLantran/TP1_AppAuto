a40fa443f09de431c2d4a1e7eeb22fd660232502
Mathilde <raby.m@outlook.fr>
2023-09-14 15:31:31 +0200

    Refactoring recherche variable

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
 import csv
+import readingFileUtils
 file_path = "AirplaneCrashes.csv"
 
-## fonction
-def getVariable(i, j):
-    with open(file_path, 'r', newline='') as file:
-        csv_reader = csv.reader(file)
-        
-        # Skip to the desired row (i-1 times)
-        for _ in range(i - 1):
-            next(csv_reader)
-        
-        # Read the row you're interested in
-        selected_row = next(csv_reader, None)
-        
-        if selected_row is not None:
-            # Check if the selected row has enough columns
-            if j <= len(selected_row):
-                # Retrieve the j-th column (j-1 index)
-                selected_cell = selected_row[j - 1]
-                print(f"Value at row {i}, column {j}: {selected_cell}")
-                return(selected_cell)
-            else:
-                print(f"Row {i} does not have {j} columns.")
-        else:
-            print(f"Row {i} does not exist in the CSV file.")
-#main
+# main
 
 i = 2
 j = 3
 try:
-    getVariable(i, j)
+    readingFileUtils.getVariable(i, j, file_path)
 except FileNotFoundError:
     print("File not found or cannot be created.")
 except PermissionError:
     print("Permission denied to create or write to the file.")
 except Exception as e:
     print(f"An error occurred: {e}")
-
-
-
-
-
-
