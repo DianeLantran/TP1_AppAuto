680533a883f8f50d562724f42564952c711cb071
DianeLantrann <diane.lantran@gmail.com>
2023-09-26 08:47:38 -0400

    FEAT : ajout algorithme SelectKBest

--- main.py
+++ main.py
@@ -6,24 +6,12 @@ import pandas as pd
 from tabulate import tabulate
 import preprocessing as prep
 
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif  # Vous pouvez utiliser une autre fonction de score
+
 FILE_PATH = "AirplaneCrashes.csv"
 DATASET = pd.read_csv(FILE_PATH)
 
-#fonction pour lire les données
-i = 2
-j = 3
-try:
-    readingFileUtils.getVariable(i, j, FILE_PATH)
-except FileNotFoundError:
-    print("File not found or cannot be created.")
-except PermissionError:
-    print("Permission denied to create or write to the file.")
-except Exception as e:
-    print(f"An error occurred: {e}")
-    
-    
-
-    
 # nettoyage des données (<70% de données sur lignes et colones)
 df = dataTreatmentUtils.removeUselessColumns(DATASET, 30)
 df = df.drop("Summary", axis=1)
@@ -42,4 +30,10 @@ df = prep.colToOrdinal(df, ["Location", "Operator",
 df = prep.standardize(df)
 
 # PCA
-sorted_eigenvectors = mathsUtils.PCA(df, 0.05) #les valeurs propres < 5% ne sont pas prises en compte
+reducData_PCA = mathsUtils.PCA(df, 0.05) #les valeurs propres < 5% ne sont pas prises en compte
+print(reducData_PCA)
+
+#Comparaison avec SelectKBest
+k_best = SelectKBest(score_func=f_classif, k=len(df.columns))  # k = nombre de caractéristiques souhaité
+reducData_SB = k_best.fit_transform(df, None) #étiquettes de classe cibles = None -> non supervisé
+print(reducData_SB)
