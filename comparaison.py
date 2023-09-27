08a07e93551beb0a6ca2e51a0bf2669a2ce842c1
Mathilde <raby.m@outlook.fr>
2023-09-27 22:40:36 +0200

    comparaison propre

--- comparaison.py
+++ comparaison.py
@@ -1,21 +1,36 @@
-import tkinter as tk
-from tkinter import scrolledtext
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def colonnes(df):
+    """
+    Print the number of columns in the dataSet
+
+    Argument: 
+    df: Numpy Array
+
+    Returns: 
+    df: Numpy Array
+    """
     nombre_de_colonnes = df.shape[1]
     print("Nombre de colonnes :", nombre_de_colonnes)
 
     return
 
 
-def correlation(df, titre):
+def correlation(df, title):
+    """
+    Plot the correlation matrix of the dataSet
+
+    Arguments: 
+    df: Numpy Array
+    title: caractéristics to put at the end of the title 
+    """
+    # Find the correlation matrix
     correlation_matrix = np.corrcoef(df, rowvar=False)
 
-    # Affichage matrice
+    # Display
     fig, ax = plt.subplots(figsize=(8, 6))
     heatmap = ax.matshow(correlation_matrix, cmap='coolwarm', vmin=-1, vmax=1)
     colorbar = fig.colorbar(heatmap)
@@ -23,6 +38,6 @@ def correlation(df, titre):
     ax.set_yticks(np.arange(len(correlation_matrix)))
     ax.set_xticklabels(range(1, len(correlation_matrix) + 1))
     ax.set_yticklabels(range(1, len(correlation_matrix) + 1))
-    plt.title("Correlation matrix for each column "+titre)
+    plt.title("Correlation matrix for each column "+title)
     plt.show()
     return
